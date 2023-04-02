0d25225e2fb455043414d57be94b474f449ba739
AlexMNet <emtamaldonado@gmail.com>
2023-04-01 23:06:02 -0700

    simplify footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,9 @@
 const Footer = () => {
   return (
-    <footer className="font-mona">
-      <div className="mx-auto text-sm sm:text-base px-6 py-3 lg:flex lg:items-center justify-end lg:px-8 text-white bg-neutral-950">
-        <div className="">
-          <p className="text-center lg:text-right">
-            &copy; Bitcoin Gumball Machine. All Rights Reserved.
-          </p>
-        </div>
-      </div>
+    <footer className="px-6 py-3 text-white bg-neutral-950 font-mona">
+      <p className="text-center lg:text-right">
+        &copy; Bitcoin Gumball Machine. All Rights Reserved.
+      </p>
     </footer>
   );
 };
