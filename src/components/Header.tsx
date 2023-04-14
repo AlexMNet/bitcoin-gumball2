66aeb0e5928b75e6609045fd724b24f56e83b32a
AlexMNet <emtamaldonado@gmail.com>
2023-04-13 21:24:06 -0700

    Add social media link and icons to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,6 @@
+import { SiTwitter, SiDiscord } from 'react-icons/si';
+import { TbWorldWww } from 'react-icons/tb';
+
 const Header = () => {
   return (
     <>
@@ -6,6 +9,18 @@ const Header = () => {
         GumballMachine///$SOL-{'>'}$BTC
       </h3>
       <h1 className="font-mona text-6xl font-bold mb-3">The Project Name</h1>
+      {/* Social Media Links */}
+      <div className="flex flex-row gap-3 mb-3 justify-center lg:justify-start">
+        <a href="https://twitter.com/" target="_blank">
+          <SiTwitter className=" w-6 h-6 text-gray-500 cursor-pointer hover:brightness-125" />
+        </a>
+        <a href="https://discord.com/" target="_blank">
+          <SiDiscord className=" w-6 h-6 text-gray-500 cursor-pointer hover:brightness-125" />
+        </a>
+        <a href="#" target="_blank">
+          <TbWorldWww className=" w-6 h-6 text-gray-500 cursor-pointer hover:brightness-125" />
+        </a>
+      </div>
       <p className="font-monaLight text-gray-400">
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi cum
         expedita consequuntur numquam non aut porro. Vel explicabo mollitia
